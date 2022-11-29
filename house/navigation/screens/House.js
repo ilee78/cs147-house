d98618ad1309d99d6738f14c1795fda8e5ca0d7f
Annabelle <75515165+ananabellewang@users.noreply.github.com>
2022-11-28 21:31:53 -0800

    Create House.js

---
+++ house/navigation/screens/House.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+
+function Profile({navigation}) {
+    return(
+        <View>
+            <Text>profile screen</Text>
+        </View>
+    );
+}
+
+export default Profile;
